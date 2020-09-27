93075b7b8040d6650d93e1ae32c0ae4aa9fc7ae0
Brian Song <44029531+briansong524@users.noreply.github.com>
2020-09-26 19:22:59 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,15 @@
 import os
-import sys
-if sys.version_info[0] == 2:
-	import thread
-else:
-	import _thread as thread
-
 import json
 import argparse
 import pickle
 
 import pandas as pd
-import numpy as np
-
 from flask import Flask, request
 
 
 app = Flask(__name__)
+
+## arguments to modify when running the script
 
 parser = argparse.ArgumentParser()
 
@@ -27,24 +21,21 @@ parser.add_argument(
 	'--risk_threshold', type=float, default = 0.5,
 	help = 'threshold from 0-1 where above means risky')
 
+parser.add_argument(
+	'--port', type=int, default= 8000,
+	help = 'What port to host API server on.')
 
-FLAGS, unparsed = parser.parse_known_args()
 
-if FLAGS.model_dir == '':
-	dir_ = os.path.dirname(os.path.realpath(__file__))
-else:
-	dir_ = FLAGS.model_dir
 
-global model, cat_dict
-with open(dir_ + '/model.pkl', 'rb') as model_in:
-	model = pickle.load(model_in)
-
-with open(dir_ + '/cat_dict.pkl', 'rb') as file_in:
-	cat_dict = pickle.load(file_in)
-
+## define URLs 
 @app.route('/heart-beat', methods=['GET'])
-def run_heart_beat_check():  
-	status_object = {'status':'alive'}
+def run_heart_beat_check():
+	try:
+		text = str(request.args.get('text',None))
+	except:
+		text ='no param set to "text"'
+	status_object = {'status':'alive',
+					 'text':text}
 	response = app.response_class(
 		response=json.dumps(status_object),
 		status=200,
@@ -56,6 +47,7 @@ def run_heart_beat_check():
 def api():
 	global model, cat_dict
 
+	# read GET parameters
 	gender = str(request.args.get('gender',None))
 	own_car = str(request.args.get('own_car',None))
 	own_home = str(request.args.get('own_home',None))
@@ -75,7 +67,7 @@ def api():
 								'AMT_INCOME_TOTAL':income}, index = [0])
 	input_data = input_data[['CNT_CHILDREN','AMT_INCOME_TOTAL','CODE_GENDER',
 							'FLAG_OWN_CAR','FLAG_OWN_REALTY']] #reorder columns
-	print(input_data)
+	
 	pred = model.predict(input_data)[0] 
 	pred_class = 'risky' if pred > FLAGS.risk_threshold else 'not_risky'
 	output = {
@@ -86,4 +78,21 @@ def api():
 
 
 if __name__ == '__main__':
-	app.run(debug=False, port=8000, host='0.0.0.0')
+
+	FLAGS, unparsed = parser.parse_known_args()
+
+	## load files
+	if FLAGS.model_dir == '':
+		dir_ = os.path.dirname(os.path.realpath(__file__))
+	else:
+		dir_ = FLAGS.model_dir
+
+	global model, cat_dict
+	with open(dir_ + '/model.pkl', 'rb') as model_in:
+		model = pickle.load(model_in)
+
+	with open(dir_ + '/cat_dict.pkl', 'rb') as file_in:
+		cat_dict = pickle.load(file_in)
+
+
+	app.run(debug=False, port=FLAGS.port, host='0.0.0.0')
